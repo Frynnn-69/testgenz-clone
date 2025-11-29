cd8bd36577e02d4121ba54aa4cf3b9577cbb2a4c
Yeyodra <bongahbotak@gmail.com>
2025-11-29 10:35:11 +0700

    Fix Vercel export-detail.json bug workaround

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  /* Disable static export to fix Vercel build error */
+  output: undefined,
 }
 
 export default nextConfig  
